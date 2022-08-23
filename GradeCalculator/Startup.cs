02b164e014c46644b4e629bf5c7f92f520ba87b2
Mark Josmil Degamo <MDegamo01@lear.com>
2022-08-23 09:48:16 +0800

    Add project files.

---
+++ GradeCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GradeCalculator.Startup))]
+namespace GradeCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
